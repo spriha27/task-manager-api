1b46a4f31b2b911709c04a074153b98abef9b820
spriha27 <spriha27@gmail.com>
2022-06-14 10:52:22 +0530

    Added display message

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) => {
+  res.send(`Please visit <a href = "https://github.com/spriha27/task-manager-api">https://github.com/spriha27/task-manager-api</a> to find REST APIs that can be used on this project using Postman.`)
+})
+
 app.listen(port, () => {
     console.log("Server is up on port " + port);
 })
